8b1620eac903afc1a590849c33d5dcd5d8466492
Roaa K <ruaakhuldoon.26@gmail.com>
2024-10-02 15:10:40 +0300

    FORMAT AND STORE DATA INTO AN ARRAY

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,28 +1,37 @@
+import { nanoid } from 'nanoid';
 import { useEffect, useState } from 'react';
 import React from 'react'
  
 function Game( ) {
   const [quizData, setQuizData]= useState([])
+  const [formattedQuestions,setFormattedQuestions] = useState([])
   useEffect(()=>
     {
       fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then((response) => response.json())
       .then((data) => {
         setQuizData(data.results);
-        
-        const fq=data.results[0]
-        console.log(fq)
-        const allAnswers=[
-          ...fq.incorrect_answers,
-          fq.correct_answer
-        ];
-        console.log(allAnswers)
-        const shuffledAnswers= shuffle(allAnswers)
-        console.log(shuffledAnswers)
-      });
+        formatQuestion(data.results)
+       
+     });
     },[])
    
+    function formatQuestion(array){
+      setFormattedQuestions(
+        array.map((questionObject) => {
+        return{
+          id: nanoid(),
+          question: questionObject.question,
+          shuffledAnswers: shuffle(
+            [...questionObject.incorrect_answers,
+            questionObject.correct_answer]),
+          correctAnswer: questionObject.correct_answer,
+          selectedAnswer: ""
+        };
+      }
 
+      ))
+    }
     function shuffle(array) {
       let currentIndex = array.length;
       while (currentIndex != 0) {
@@ -32,8 +41,10 @@ function Game( ) {
         [array[currentIndex], array[randomIndex]] = [
           array[randomIndex], array[currentIndex]];
       }
+      return array;
     }
      
+  console.log(formattedQuestions)
   return (
     <div> Game</div>
     
